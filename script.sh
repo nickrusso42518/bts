d4c0ec97dfb4d35b691383662b577d7f20b704a5
nick <nickrus@cisco.com>
2021-06-08 05:38:04 -0400

    WIP #2 sleep added

--- script.sh
+++ script.sh
@@ -8,5 +8,8 @@ set -x
 # Start exabgp on the localhost background with dummy port (doesn't need root)
 env exabgp.tcp.bind=127.0.0.1 exabgp.tcp.port=5001 exabgp $1/ci.txt &
 
+# Wait 3 seconds for server to completely start to avoid false negatives
+sleep 3
+
 # Run the API test cases in the proper directory
 pytest --verbose $1/test_api.py
